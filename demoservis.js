db5455420f6829234b9a1f6aad1b6c1dc51db246
okansafak <okn.safak@gmail.com>
2025-07-24 12:38:54 +0300

    İlk yükleme: OpenLayers Map Viewer

---
+++ demoservis.js
@@ -0,0 +1,7 @@
+// Demo servis adresleri
+export const DEMO_SERVICES = {
+  wms: "https://view.eumetsat.int/geoserver/ows?service=WMS",
+  wfs: "https://view.eumetsat.int/geoserver/ows?service=WFS",
+  mapserver: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/OilSandsProjectBoundaries/MapServer",
+  featureserver: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/OilSandsProjectBoundaries/FeatureServer"
+};
